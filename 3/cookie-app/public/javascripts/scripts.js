0429bbb1a9efdc275497bffd2f8f7acb314c3867
miwnKEA <mwn.digi@cbs.dk>
2025-09-16 00:03:49 +0200

    updated js

---
+++ 3/cookie-app/public/javascripts/scripts.js
@@ -0,0 +1,103 @@
+async function getUsers() {
+  const response = await fetch("/user");
+  const data = await response.json();
+  alert(JSON.stringify(data));
+}
+
+async function getUser() {
+  const response = await fetch("/user/mikkel");
+  const data = await response.json();
+  alert(JSON.stringify(data));
+}
+
+async function createUser() {
+  const response = await fetch("/user/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: "Hildur",
+      password: "hildudigi",
+      email: "hil.digi@cbs.dk",
+    }),
+  });
+  const data = await response.json();
+  alert(JSON.stringify(data));
+}
+
+async function updateUser() {
+  const response = await fetch("/user/update/Hildur", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ password: "hildudigidoo" }),
+  });
+  const data = await response.json();
+  alert(JSON.stringify(data));
+}
+
+async function deleteUser() {
+  const response = await fetch("/user/delete/Hildur", {
+    method: "DELETE",
+  });
+  const data = await response.json();
+  alert(data.message);
+}
+
+async function setCookie() {
+  const response = await fetch("/cookie/set");
+  const data = await response.json();
+  alert(data.message);
+}
+
+async function getCookie() {
+  const response = await fetch("/cookie/get");
+  const data = await response.json();
+  alert(data.message);
+}
+
+async function middleware() {
+  const response = await fetch("/middleware");
+  const data = await response.json();
+  alert(JSON.stringify(data));
+}
+
+async function setSessionCookie() {
+  const response = await fetch("/cookie/set-session");
+  const data = await response.json();
+  alert(data.message);
+}
+
+async function getSessionCookie() {
+  const response = await fetch("/cookie/get-session");
+  const data = await response.json();
+  alert(data.message);
+}
+
+async function login() {
+  const response = await fetch("/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username: "mikkel" }),
+  });
+  const data = await response.json();
+  alert(JSON.stringify(data));
+}
+
+async function logout() {
+  const response = await fetch("/auth/logout", {
+    method: "POST",
+  });
+  const data = await response.json();
+  alert(JSON.stringify(data));
+}
+
+async function protected() {
+  const response = await fetch("/auth/protected");
+  const data = await response.json();
+  alert(JSON.stringify(data));
+}
