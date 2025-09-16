a7cdf8e383aae5f1bd052e6a9185b67f8df2f49a
miwnKEA <mwn.digi@cbs.dk>
2025-09-16 10:21:44 +0200

    image

--- 3/cookie-app/routes/index.js
+++ 3/cookie-app/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 /* GET forside */
 router.get('/', async (req, res) => {
   res.render('index', { title: 'Cookie App' });
+});
+
+router.get('/image', async (req, res) => {  
+  // Set cache-control and pragma headers to prevent caching
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+  res.setHeader('Pragma', 'no-cache');
+  res.sendFile(path.join(__dirname, "../public/images", "cbs.jpeg"));
 });
 
 module.exports = router;
